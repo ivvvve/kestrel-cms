f3097e90a197b08b2679f1e6119f61eb4aad67cf
Ed Ive <ed.ive@unitetheunion.org>
2021-07-01 15:09:02 +0100

    add contact page to model

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
-from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.blocks import ImageChooserBlock
@@ -10,13 +10,6 @@ from wagtail.embeds.blocks import EmbedBlock
 
 class RegionPage(Page):
 
-    twitter_handle = models.CharField(max_length=200)
-    body = StreamField([
-        ('heading', blocks.CharBlock(form_classname="full title")),
-        ('paragraph', blocks.RichTextBlock()),
-        ('image', ImageChooserBlock()),
-        ('embed', EmbedBlock())
-    ])
     header_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -24,10 +17,44 @@ class RegionPage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock())
+    ],
+    blank=True
+    )
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('header_image'),
-        FieldPanel('twitter_handle'),
+        StreamFieldPanel('body')
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+    ]
+
+class ContactPage(Page):
+
+    header_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock())
+    ],
+    blank=True
+    )
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('header_image'),
         StreamFieldPanel('body')
     ]
 
