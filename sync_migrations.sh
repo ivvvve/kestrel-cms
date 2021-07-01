7b6956568e99a0707783d4fd25ffad127d9fa0ae
Ed Ive <ed.ive@unitetheunion.org>
2021-07-01 13:27:11 +0100

    simplify model sync script

--- sync_migrations.sh
+++ sync_migrations.sh
@@ -1,2 +1,6 @@
-WEB_ID=$(docker ps -aqf name=kestrel-cms_web)
-docker cp $WEB_ID:/app/home/migrations ./home
+WEB_ID=$(docker ps -aqf name=kestrel-cms_web);
+
+docker exec kestrel-cms_web_1 rm -rf /app/home;
+docker cp ./home $WEB_ID:/app/home;
+docker exec kestrel-cms_web_1 python3 manage.py makemigrations;
+docker cp $WEB_ID:/app/home/migrations ./home;
