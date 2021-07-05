60a19cf4e97cc2f88681d5682a95119b98a2ac3e
Ed Ive <ed.ive@unitetheunion.org>
2021-07-05 16:54:19 +0100

    housekeeping: update watch-template to also cover css

--- copy_templates.sh
+++ copy_templates.sh
@@ -2,3 +2,5 @@ WEB_ID=$(docker ps -aqf name=kestrel-cms_web);
 
 docker exec kestrel-cms_web_1 rm -rf /app/home/templates;
 docker cp ./home/templates $WEB_ID:/app/home/templates;
+docker exec kestrel-cms_web_1 rm -rf /app/home/static;
+docker cp ./home/static $WEB_ID:/app/home/static;
